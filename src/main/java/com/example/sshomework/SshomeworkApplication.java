54e1541350be33d6f24964160a2f66911d302214
Romodin Aleksey <workbegin@gmail.com>
2020-11-07 12:13:21 +0400

    Create repo

---
+++ src/main/java/com/example/sshomework/SshomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.example.sshomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SshomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SshomeworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sshomework/SshomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sshomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SshomeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
